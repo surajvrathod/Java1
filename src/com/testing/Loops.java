c9baf135af5f5f6e86976b257c074d4ea7cb1456
suraj <surajrathodofficial@gmail.com>
2024-03-19 18:16:38 +0530

    Initial commit

---
+++ src/com/testing/Loops.java
@@ -0,0 +1,16 @@
+package com.testing;
+
+public class Loops {
+    public  static  void  main(String[] args){
+        System.out.println("Welcome to Loops Class");
+        //Loops are used to do repititive tasks
+        //If Else if
+        byte a=5;
+        if(a%2==0)
+            System.out.printf("%d is a even Number",+a);
+        else if(a==3)
+            System.out.printf("%d is equals 3",+a);
+        else
+            System.out.println("It's Default case");
+    }
+}
