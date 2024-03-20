ff309abfcbd7c72df558163f095d1d9963172b69
suraj <surajrathodofficial@gmail.com>
2024-03-20 17:54:47 +0530

    Merge remote-tracking branch 'origin/master'

--- src/com/testing/Arrays.java
+++ src/com/testing/Arrays.java
@@ -28,6 +28,26 @@ public class Arrays {
             System.out.print(element+" ");
         }
 
+        //Multidimentional Arrays
+        //2D arrays
+        System.out.println();
+        System.out.println("Multidimentinal Array");
+        int [] []flats=new int[2] [3];
+        flats [0][0]=101;
+        flats [0][1]=102;
+        flats [0][2]=103;
+        flats [1][0]=201;
+        flats [1][1]=202;
+        flats [1][2]=203;
+        System.out.println(flats[1][2]);
+        System.out.println("Length of Rows are "+flats.length);
+        System.out.println("Length of Columns are "+flats[0].length);
+        for (int i=0;i<flats.length;i++){
+            for (int j=0;j<flats[i].length;j++){
+                System.out.print(flats[i][j]+" ");
+            }
+            System.out.println();
+        }
 
     }
 }
