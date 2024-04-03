3ab86e08a9931f1ae61df37772d4b744a8f70a0d
suraj <surajrathodofficial@gmail.com>
2024-04-03 11:37:48 +0530

    Merge remote-tracking branch 'origin/master'

--- src/com/testing/ThreadMethods.java
+++ src/com/testing/ThreadMethods.java
@@ -2,29 +2,36 @@ package com.testing;
 
 class CWH1 extends Thread{
     public void run(){
-        try {
-            for (int i=0;i<5;i++){
-                System.out.println("Child Thread");
-                Thread.sleep(4000); /* Child thread is put to sleep for 4000ms. As soon as child thread goes to sleep main thread interrupts it. And, InterruptedException is generated which is handled by the catch block. */
 
-            }
-        }
-        catch (InterruptedException e)
+//        try {
+//            for (int i=0;i<5;i++){
+//                System.out.println("Child Thread");
+//                Thread.sleep(4000); /* Child thread is put to sleep for 4000ms. As soon as child thread goes to sleep main thread interrupts it. And, InterruptedException is generated which is handled by the catch block. */
+//
+//            }
+//        }
+//        catch (InterruptedException e)
+        while(true)
         {
             System.out.println("Child Thread Interrupted");
         }
-        System.out.println("Thread is running");
-
+//        System.out.println("Thread is running");
     }
 }
 
 public class ThreadMethods extends Thread{
-    public static void main(String[] args) {
-        CWH1 t= new CWH1();
-        t.start();
-        t.interrupt();
-        System.out.println("Main Thread");
+    public static void main(String[] args)  {
+        CWH1 t1= new CWH1();
+        CWH1 t2= new CWH1();
 
+        t1.start();
+        try {
+            t1.join();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        t2.start();
+        System.out.println("Main Thread");
     }
 }
 
