7973b02ea6a91eb8cd9d93b4d4b2fe691ed9dbb6
suraj <surajrathodofficial@gmail.com>
2024-04-09 15:47:14 +0530

    Merge remote-tracking branch 'origin/master'

--- src/com/testing/SQLDRL.java
+++ src/com/testing/SQLDRL.java
@@ -1,8 +1,5 @@
 package com.testing;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 /*
 1) Create a connection
 2) Create statement/Query
@@ -10,22 +7,28 @@ import java.sql.Statement;
 4) close connection
 */
 
-public class MYSQL {
+public class SQLDRL {
     public static void main(String[] args) throws SQLException {
         //1) Create a connection
 
         Connection con=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/TEST", "root","root");
 
 //2) Create statement/Query Statement -con.createStatement();
-        Statement stmt= con.createStatement();
+      Statement stmt= con.createStatement();
 
-            String s="INSERT INTO T1 VALUES (3,'NAM')";
-                //String s="UPDATE  STUDENT SET STUDENT_NAME='DAVID' WHERE S_NO=103";
+      //String s="INSERT INTO T1 VALUES (2,'RAJ')";
+         String s="select n,name from t1";
+        //String s="UPDATE  STUDENT SET STUDENT_NAME='DAVID' WHERE S_NO=103";
         //  String s="DELETE FROM STUDENT WHERE S_NO=107)";
 
-//3) Execute statment/Query
-
-        stmt.execute(s);
+//3) Execute statment/Query AND Store Result Set
+        //stmt.execute(s);
+        ResultSet rs=stmt.executeQuery(s);
+while (rs.next()){
+    int num=rs.getInt("N");
+      String name1=rs.getString("NAME");
+      System.out.println(num +" "+ name1);
+}
 
 //4) close connection
         con.close();
